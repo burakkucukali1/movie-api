cfd40e2ead45140678d8e5ec4c30b9a5eae94252
BK <burakkucukali6@gmail.com>
2020-05-21 02:38:47 +0300

    model validation

--- models/Director.js
+++ models/Director.js
@@ -2,15 +2,25 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const DirectorSchema = new Schema({
-     name: String,
-     surname: String,
-     bio: String,
-     createdAt: {
-          type: Date,
-          default: Date.now
-     }
-     
-     
-})
+	name: {
+		type: String,
+		maxlength: 60,
+		minlength: 2
+	},
+	surname: {
+		type: String,
+		maxlength: 60,
+		minlength: 2
+	},
+	bio: {
+		type: String,
+		maxlenght: 1000,
+		minlength: 60
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now
+	}
+});
 
 module.exports = mongoose.model('director', DirectorSchema)
